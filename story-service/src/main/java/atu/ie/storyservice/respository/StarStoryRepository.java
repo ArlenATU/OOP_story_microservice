13b6d317b693f085b7c681e0436e67fbf09b1fdd
ArlenATU <G00470377@atu.ie>
2026-04-18 20:33:51 +0100

    user microservice

--- story-service/src/main/java/atu/ie/storyservice/respository/StarStoryRepository.java
+++
@@ -1,9 +0,0 @@
-package atu.ie.storyservice.repository;
-
-import atu.ie.storyservice.model.StarStory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StarStoryRepository extends JpaRepository<StarStory, Long> {
-    List<StarStory> findByUserId(Long userId);
-}
